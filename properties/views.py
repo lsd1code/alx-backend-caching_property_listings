33f3b4bd10c8438228efc2ae2458d8658d7e6184
lesedi thibile <lesedi.xys11@gmail.com>
2025-08-29 00:17:44 +0200

    CHORE: setup django with dockerized postgresql and redis

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+from .serializers import (
+    PropertySerializer
+)
+
+from .models import (
+    Property
+)
+
+
+@api_view(['GET'])
+@cache_page(60*15)
+def property_list(request: Request):
+    queryset = Property.objects.all()
+    serializer = PropertySerializer(queryset, many=True)
+
+    return Response(serializer.data)
